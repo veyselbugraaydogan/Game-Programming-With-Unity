6dc3ed089b8a57f08671001421f1f8bc16228f57
veyselbugraaydogan <veyselbugraaydogan@gmail.com>
2019-10-14 23:56:47 +0300

    First Practice

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{	
+
+    public GameObject vehicle;
+    private Vector3 camera_pos =new Vector3(0,5,-7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = vehicle.transform.position + camera_pos;
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+
+    private float speed = 30;
+    private float horizontalInput;
+    private float verticalInput;
+    private float turnspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnspeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput * verticalInput);
+
+
+    }
+}
